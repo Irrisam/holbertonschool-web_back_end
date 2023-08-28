cff17e77db0252fe307019ac9a3bdeeda7f23d71
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-08-28 12:07:53 +0200

    add task 2

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""returns the concatenated strings but annotated"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates two strings and returns it"""
+    return (str1 + str2)
