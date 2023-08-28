3cc74b18b9c64fcc47ed1b364c0eb35bb33f3ff8
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-08-28 14:26:24 +0200

    add comments + correct return

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from typing import Callable
-""
+"""function that returns a multiplying function"""
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    def multiplier(value: float) -> float:
+    """function returning the multiplying function"""
+    def multiplier_func(value: float) -> float:
+        """the multiplying function"""
         result = value * multiplier
         return result
-    return make_multiplier
+    return multiplier_func
