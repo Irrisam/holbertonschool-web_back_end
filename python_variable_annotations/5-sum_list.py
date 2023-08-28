7c55b714db7b1a0bfad5e435b2ac720eca5e682d
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-08-28 13:38:42 +0200

    add task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""sums up a list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """sums up a list of float then returns it """
+    return sum(input_list)
