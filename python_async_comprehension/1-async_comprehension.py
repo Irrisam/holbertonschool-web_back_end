0ca69d9af83f291ae0082f25a2f2cfbe253ffb85
Irrisam <tristanduchamp@hotmail.fr>
2023-08-30 01:12:57 +0200

    try to rectify variable name to fit in checker

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,6 +9,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """generates 10 values using asynchronous loop trough async generator"""
-    return_values = [random_number async for random_number in
-                     async_generator()]
-    return return_values
+    return_value = [random_number async for random_number in async_generator()]
+    return return_value
