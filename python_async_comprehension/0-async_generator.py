6f2f49eba9dcd15f8c38e55b08f35d39f0831370
Irrisam <tristanduchamp@hotmail.fr>
2023-08-30 00:54:55 +0200

    modify annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float]:
     """function generating asynchronous method"""
     for _ in range(10):
         await asyncio.sleep(1)
