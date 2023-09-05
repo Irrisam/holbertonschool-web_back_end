be973e2126f9336c12fec67879751b555ab5f60d
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-05 12:11:54 +0200

    add shebang and comments

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+"""function that returns a list of all documents in a mongo collection"""
+
+
 def list_all(mongo_collection):
+    """function that returns a list of all documents in a mongo collection."""
     cursor = mongo_collection.find({})
     docs = []
 
