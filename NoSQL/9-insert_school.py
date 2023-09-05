6f6fe6f8181b13696631643561c30bd7b08980ee
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-05 14:40:18 +0200

    correct from pymongo to mongosh syntax

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 """adds multiple docs a collection inputed"""
 
-from pymongo import MongoClient
-
 
 def insert_school(mongo_collection, **kwargs):
     """adds multiple docs a collection inputed"""
-    client = MongoClient()
-    db = client("school")
-    collection = db[mongo_collection]
-
-    collection.insert(kwargs)
-
-    client.close
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
