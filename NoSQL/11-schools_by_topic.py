8f25ea5b61180e58782bd4c846b9827292495488
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-06 14:05:11 +0200

    add comments

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" """
+"""function that returbs all schools that have given topics."""
 
 
 def schools_by_topic(mongo_collection, topic):
-    """ """
+    """function that returbs all schools that have given topics."""
     school_names = []
 
     for school in mongo_collection.find({"topics": topic}):
