597ec24b2944d7dd5339338fe5003de9f65ca59b
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-06 13:46:28 +0200

    remove return value

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,6 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """function updating topics dependind on name"""
-    result = mongo_collection.updateMany({"name": name},
-                                         {"$set": {"topics": topics}})
-    return result
+    mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
