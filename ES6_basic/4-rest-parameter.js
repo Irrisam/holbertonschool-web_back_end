3c95ef4b271d927eb47cfbd50644a664730d9bcb
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-14 12:36:09 +0000

    simplify task 4

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
-export default function returnHowManyArguments(...var_num) {
-  let total = 0;
-  for (const arg of var_num) {
-    total += arg;
-  }
-  return total;
+export default function returnHowManyArguments(...theArgs) {
+  return theArgs.length;
 }
