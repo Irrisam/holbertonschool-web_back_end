8ff25c059f30295c9269cd37e8174f0ddabef1cb
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-02 09:12:23 +0000

    modified task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,17 +1,9 @@
-function getResponseFromAPI(yes) {
-    return new Promise((resolve, reject) => {
-        if (yes) {
-            resolve();
-        } else {
-            reject();
-        }
-    });
+export default function getResponseFromAPI(True) {
+  return new Promise((resolve, reject) => {
+      if (True) {
+          resolve("yay");
+      } else {
+          reject("ohhh nooo");
+      }
+  });
 }
-
-getResponseFromAPI(true)
-    .then(() => {
-        console.log("Yup");
-    })
-    .catch(() => {
-        console.log("Nope");
-    });
