42f8621cfce3b3fcb2afb027fd4d32197e052426
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-04 10:28:29 +0000

    correct ESLINT

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,12 @@
 export default function updateStudentGradeByCity(paramArray, city, newGrades) {
-    const citiesArray = paramArray.filter((student) => student.location === city);
-    const finalArray = citiesArray.map((student) => {
-      const searchsGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
-  
-      if (searchsGrade && searchsGrade.grade !== undefined) {
-        return { ...student, grade: searchsGrade.grade };
-      } else {
-        return { ...student, grade: "N/A" };
-      }
-    });
-    return finalArray;
-  }
-  
+  const citiesArray = paramArray.filter((student) => student.location === city);
+  const finalArray = citiesArray.map((student) => {
+    const searchsGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+
+    if (searchsGrade && searchsGrade.grade !== undefined) {
+      return { ...student, grade: searchsGrade.grade };
+    }
+    return { ...student, grade: 'N/A' };
+  });
+  return finalArray;
+}
