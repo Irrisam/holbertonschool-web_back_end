e9472cfc9d394ea42cc9053df3c0b97a759f80a0
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-04 15:16:39 +0000

    uncomplete task 8, still split each letters instea

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,15 @@
 export default function cleanSet(paramSet, startString) {
-  const returnString = [];
-  const paramArray = Array.from(paramSet);
-  if (startString !== '') {
-    for (const item of paramArray) {
-      if (item.startsWith(startString)) {
-        returnString.push(item);
-      }
+    const returnString = [];
+    const paramArray = Array.from(paramSet);
+
+    if (startString !== '') {
+        for (const item of paramArray) {
+            if (item.startsWith(startString)) {
+                const withoutStart = item.substring(startString.length);
+                returnString.push(withoutStart.split('').join('-')); // Split and join with hyphen (-)
+            }
+        }
     }
-  }
-  return returnString;
+
+    return returnString;
 }
