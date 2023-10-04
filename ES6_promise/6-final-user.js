6e7c38478b03a3798266a960aaf7e24751824686
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-04 08:28:54 +0000

    add peer stategy for error handling

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,10 +6,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => {
-      const fulfilledResults = results.filter((result) => result.status === 'fulfilled');
-      const values = fulfilledResults.map((result) => result.value);
-
-      return values;
-    });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })));
 }
