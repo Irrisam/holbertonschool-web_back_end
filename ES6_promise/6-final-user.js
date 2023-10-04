75700ac29511081e3385a1dc86355c5a6e1fcb3a
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-04 08:26:01 +0000

    try .filter method

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-}
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const fulfilledResults = results.filter((result) => result.status === 'fulfilled');
+      const values = fulfilledResults.map((result) => result.value);
+
+      return values;
+    });
+}
