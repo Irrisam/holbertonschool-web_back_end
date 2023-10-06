9d7cda094d99d69b0141b72030e809c67dd2beec
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-06 11:51:59 +0000

    cgange file 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,26 @@
-export default function cleanSet(paramSet, startString) {
-  const returnString = [];
-  const paramArray = Array.from(paramSet);
+export default function cleanSet(inputSet, startString) {
+  const emptyStr = '';
+  if (typeof startString !== 'string') {
+    return emptyStr;
+  }
+
   if (startString !== '') {
-    for (const item of paramArray) {
-        if (item.startsWith(startString)){
-        const withoutStart = item.substring(startString.length);
-        returnString.push(withoutStart.split('-').join(''));
+    // Convert the Set to an array for easier manipulation
+    const setArray = Array.from(inputSet);
+    while (setArray.indexOf(undefined) !== -1) {
+      const index = setArray.indexOf(undefined);
+      setArray.splice(index, 1);
     }
-}
-}
-  return returnString;
+    // Use the filter() method to keep only the values that start with startString
+    const filteredArray = setArray.filter((value) => value.startsWith(startString));
+
+    // Use the map() method to extract the part of the string after startString
+    const cleanedArray = filteredArray.map((value) => value.substring(startString.length));
+
+    // Join the cleanedArray elements with "-"
+    const resultString = cleanedArray.join('-');
+
+    return resultString;
+  }
+  return emptyStr;
 }
