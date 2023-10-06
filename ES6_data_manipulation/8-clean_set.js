28c06e9b876e3767f5c4fc6d8e6fdb443e50f7d8
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-06 08:02:44 +0000

    add task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,14 @@
 export default function cleanSet(paramSet, startString) {
-    const returnString = [];
-    const paramArray = Array.from(paramSet);
-
-    if (startString !== '') {
-        for (const item of paramArray) {
-            if (item.startsWith(startString)) {
-                const withoutStart = item.substring(startString.length);
-                returnString.push(withoutStart.split('').join('-')); // Split and join with hyphen (-)
-            }
-        }
+  const returnString = [];
+  const paramArray = Array.from(paramSet);
+  if (startString !== '') {
+    for (const item of paramArray) {
+        if (item.startsWith(startString)){
+        const withoutStart = item.substring(startString.length);
+        returnString.push(withoutStart.split('-').join(''));
+        returnString.toString();
     }
-
-    return returnString;
+}
+}
+  return returnString;
 }
