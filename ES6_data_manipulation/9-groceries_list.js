bddc0c62c0be48840fb5aa76577979bccead86ba
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-06 08:16:56 +0000

    add eslint

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-    let dataMap = {
-        Apples: 10,
-        Tomatoes: 10,
-        Pasta: 1,
-        Rice: 1,
-        Banana: 5,
-    };
-    return dataMap;
+  const dataMap = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
+  return dataMap;
 }
