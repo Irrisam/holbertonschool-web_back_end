cd59876286075bf5b234bcf254d03254c3c8ce8e
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-06 08:22:11 +0000

    add task 9

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,9 @@
 export default function groceriesList() {
-  const dataMap = {
-    Apples: 10,
-    Tomatoes: 10,
-    Pasta: 1,
-    Rice: 1,
-    Banana: 5,
-  };
+    const dataMap = new Map();
+    dataMap.set('Apples', 10);
+    dataMap.set('Tomatoes', 10);
+    dataMap.set('Pasta', 1);
+    dataMap.set('Rice', 1);
+    dataMap.set('Banana', 5);
   return dataMap;
 }
